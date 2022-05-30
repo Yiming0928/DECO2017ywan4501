c9b242eebf5740a4aff59ae7d7d64eaba83e266f
Yiming0928 <1357666087@qq.com>
2022-05-30 22:32:30 +1000

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ShareModule } from './module/share.module';
-import { NgDragDropModule } from 'ng-drag-drop';
+
 import { AppComponent } from './app.component';
 import { IconsProviderModule } from './icons-provider.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    TaskListComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +18,6 @@ import { TaskListComponent } from './components/task-list/task-list.component';
     HttpClientModule,
     BrowserAnimationsModule,
     ShareModule,
-    NgDragDropModule.forRoot(),
   ],
   bootstrap: [AppComponent]
 })
